f2ec550af752262db6801c5abb950c2f17b682eb
Siarhei Savitski <sv.siarhei@gmail.com>
2016-12-22 00:56:33 +0300

    Extend data view component

--- app/components/data-view.js
+++ app/components/data-view.js
@@ -1,17 +1,32 @@
 import Ember from 'ember';
+import _ from 'lodash/lodash';
 
 export default Ember.Component.extend({
+	store: Ember.inject.service(),
+
     actions: {
-        onAddItem() {
-            console.log('Item has been added');
+        onDeleteItem(item) {
+        	let store = this.get('store');
+            let data = this.get('data');
+            let id = item.get('id');
+            let _self = this;
+        	
+        	store.findRecord('data-item', id).then((item) => {
+        		item.deleteRecord();
+        		item.save();
+                _.each(data, (item, index) => {
+                    if (_.isMatch(item, { id })) {
+                        data.splice(index, 1);
+                    }
+                });
+                _self.set('data', data);
+                _self.itemChanged(id);
+        		console.log('Action > Item has been deleted');
+        	});
         },
+    },
 
-        onEditItem() {
-            console.log('Item has been edited');
-        },
-
-        onDeleteItem() {
-            console.log('Item has been deleted');
-        },
-    }
+    itemChanged: function(id) {
+        this.$().find(`#${id}`).remove();
+    },
 });
